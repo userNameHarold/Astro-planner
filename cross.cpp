ba87a7d27af11cc1b478d8f11dc17caacdcfbf60
userNameHarold <93227278+userNameHarold@users.noreply.github.com>
2023-02-15 13:52:48 -0700

    Add files via upload

--- cross.cpp
+++ cross.cpp
@@ -4,7 +4,7 @@
 #include <vector>
 
 std::vector<double> cross(const std::vector<double> *v1, const std::vector<double> *v2) {
-    std::vector<double> result;
+    std::vector<double> result(3);
     result[0] = (*v1)[1] * (*v2)[2] - (*v1)[2] * (*v2)[1];
     result[1] = (*v1)[2] * (*v2)[0] - (*v1)[0] * (*v2)[2];
     result[2] = (*v1)[0] * (*v2)[1] - (*v1)[1] * (*v2)[0];
--- dot.cpp
+++ dot.cpp
@@ -3,19 +3,6 @@
 
 #include <vector>
 
-/* void dot(const double *matrix1, const double *matrix2, double *result, int rows1, int cols1, int cols2) {
-    for (int i = 0; i < rows1; ++i) {
-        for (int j = 0; j < cols2; ++j) {
-            double dotProduct = 0.0;
-            for (int k = 0; k < cols1; ++k) {
-                dotProduct += matrix1[i*cols1 + k] * matrix2[k*cols2 + j];
-            }
-            result[i*cols2 + j] = dotProduct;
-        }
-    }
-} */
-
-
 double dot(const std::vector<double> *vec1, const std::vector<double> *vec2) {
     return (*vec1)[0] * (*vec2)[0] + (*vec1)[1] * (*vec2)[1] + (*vec1)[2] * (*vec2)[2];
 } 
