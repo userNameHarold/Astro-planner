cf67a97bc52eca07317e87710f62fecb28f8f486
userNameHarold <93227278+userNameHarold@users.noreply.github.com>
2023-02-19 17:44:14 -0700

    Add files via upload

---
+++ vec3Mag.hpp
@@ -0,0 +1,17 @@
+#ifndef vec3Mag_h
+#define vec3Mag_h
+
+
+#include <glm/ext/vector_double3.hpp> // for dvec3 type, 3-component vectors of double precision
+
+double vec3Mag(glm::dvec3 v){
+	double sum = 0;
+	
+	for(int i = 0; i < 3; ++i){
+		sum += v[i] * v[i];
+	}
+	return sqrt(sum);
+
+}
+
+#endif
