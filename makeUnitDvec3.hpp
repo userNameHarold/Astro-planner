b741ce91fb74d66e90c1f6acf1bcb03f9c87448a
userNameHarold <93227278+userNameHarold@users.noreply.github.com>
2023-02-19 18:05:23 -0700

    Add files via upload

---
+++ makeUnitDvec3.hpp
@@ -0,0 +1,18 @@
+#ifndef makeUnitDvec3_h
+#define makeUnitDvec3_h
+
+
+#include <glm/ext/vector_double3.hpp> // for dvec3 type, 3-component vectors of double precision
+#include "dvec3Mag.hpp" // for getting vector magnitude 
+
+glm::dvec3 makeUnitDvec3(glm::dvec3 v){
+	double magnitude = dvec3Mag(v);
+	
+	for(int i = 0; i < 3; ++i){
+		v[i]= v[i] / magnitude;
+	}
+	return v;
+
+}
+
+#endif
